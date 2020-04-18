102cf31043025502d79081e70f019e6689efa538
Naufil <m.naufil1@gmail.com>
2020-04-18 16:57:26 +0500

    second commit

--- app.js
+++ app.js
@@ -3,37 +3,34 @@ const app = express()
 const mongoose = require("mongoose")
 let body_parser = require("body-parser")
 let User = require("./user.model")
+app.use(express.json()); // Make sure it comes back as json
 app.use(body_parser.json())
 app.use(body_parser.urlencoded({
-    extended:true
+    extended: true
 }));
 const db = "mongodb+srv://mnaufil:<password>@cluster0-wuuzb.mongodb.net/covid19?retryWrites=true&w=majority";
-mongoose.connect(db,{ useNewUrlParser: true},()=>{
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("connected to mongodb");
 });
-app.post("/",(req,res)=>{
-    let new_user = new User();
-    new_user.research_consent = req.body.research_consent;
-    // new_user.condition = req.body.condition;
-    // new_user.age_group = req.body.age_group;
-    // new_user.gender = req.body.gender;
-    // new_user.patient_id = req.body.patient_id;
-
-    // new_user.smoker = req.body.smoker;
-    // new_user.reported_symptoms = req.body.reported_symptoms;
-    // new_user.medical_history = req.body.medical_history;
-    // new_user.data_collected_using_smartphone = req.body.data_collected_using_smartphone;
+app.post("/", (req, res) => {
+    let new_user = new User({
+        research_consent: req.body.research_consent,
+        condition : req.body.condition,
+        age_group : req.body.age_group,
+        gender : req.body.gender,
+        patient_id : req.body.patient_id,
+        smoker : req.body.smoker,
+        reported_symptoms : req.body.reported_symptoms,
+        medical_history : req.body.medical_history,
+        data_collected_using_smartphone : req.body.data_collected_using_smartphone,
+    });
     console.log(new_user);
-    new_user.save(function(err,user){
-        if (err){
-            res.send("Error in saving a user");
-        }
-        else {
-            console.log(user);
-            res.send(user);
-        }
-    })
+    new_user.save()
+        .then((user) => res.send(user))
+        .catch((err) => console.log(err))
+    console.log("heyyyy {}", new_user);
+
 })
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("I am active")
 })
--- user.model.js
+++ user.model.js
@@ -1,15 +1,36 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 let user_schema = new Schema({
-    research_consent: String,
-    // condition: String,
-    // age_group: String,
-    // gender: String,
-    // patient_id: String,
-    // smoker: String,
-    // reported_symptoms: String,
-    // medical_history: String,
-    // data_collected_using_smartphone: String,
-    // date: Date.now()
+    research_consent: {
+        type: String
+    },
+    condition: {
+        type: String,
+    },
+    age_group: {
+        type: String,
+    },
+    gender: {
+        type: String
+    },
+    patient_id: {
+        type: String
+    },
+    smoker: {
+        type: String
+    },
+    reported_symptoms: {
+        type: String
+    },
+    medical_history: {
+        type: String
+    },
+    data_collected_using_smartphone: {
+        type: String
+    },
+    time : { 
+        type : Date, 
+        default: Date.now 
+    }
 });
 module.exports = mongoose.model("User",user_schema)
