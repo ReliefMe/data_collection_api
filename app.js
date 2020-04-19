cef4944d8f1327811818b90a8c4e6c3500d872f4
Naufil <m.naufil1@gmail.com>
2020-04-19 18:02:53 +0500

    uploading files added functionality

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const file_upload = require("express-fileupload")
 let body_parser = require("body-parser")
 let User = require("./user.model")
+
 app.use(express.json()); // Make sure it comes back as json
 app.use(body_parser.json())
 app.use(body_parser.urlencoded({
     extended: true
 }));
+app.use(file_upload());
+
 const db = "mongodb+srv://mnaufil:<password>@cluster0-wuuzb.mongodb.net/covid19?retryWrites=true&w=majority";
+
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("connected to mongodb");
 });
+
 app.post("/add_user", (req, res) => {
     let new_user = new User({
         research_consent: req.body.research_consent,
@@ -29,6 +35,34 @@ app.post("/add_user", (req, res) => {
         .catch((err) => console.log(err))
 
 })
+
+app.post("/cough_upload",(req,res)=>{
+    console.log(req.files);
+    let audio = req.files.audio;
+    audio.mv("./cough/"+audio.name,function(err,result){
+        if (err){
+            res.send({
+                message: "An error occured"
+            })}
+            else {
+                res.send({message: "File uploaded"})
+            }
+        })
+    })
+
+app.post("/breath_upload",(req,res)=>{
+        let audio = req.files.audio;
+        audio.mv("./breath/"+audio.name,function(err,result){
+            if (err){
+                res.send({
+                    message: "An error occured"
+                })}
+                else {
+                    res.send({message: "File uploaded"})
+                }
+            })
+        })
+
 app.listen(3000, () => {
     console.log("I am active")
 })
